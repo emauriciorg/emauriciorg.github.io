3b6cb93dfb74da6e881fa5d62929b06d99c23d30
emauriciorg <emauriciorg@gmail.com>
2020-11-01 22:56:12 -0500

    fix(currentcy exchange) : added https API to be compatible with heroku app

--- js/exchange.js
+++ js/exchange.js
@@ -14,12 +14,13 @@ function perform_fetch(currency_one,currency_two){
 	console.log( currency_two);
 	console.log(currency_one);
 	
-	fetch(`http://data.fixer.io/api/latest?access_key=30ce2048ab5b3b135a6fbdd05796af00&symbols=${currency_one},${currency_two}`, {
+	
+	fetch(`https://v6.exchangerate-api.com/v6/5612e6a5900febde9af0225a/latest/${currency_one}`, {
 		    method: "GET"
 		  }).then(res => res.json())
 		  .then(data =>{
 			console.log(data); 
-			const rate= data.rates[currency_two];
+			const rate= data.conversion_rates[currency_two];
 			console.log(rate);
 			rateEl.innerText=`1 ${currency_one} = ${rate} ${currency_two}`;
 	});
